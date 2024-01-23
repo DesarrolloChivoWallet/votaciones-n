7c398ac6b50c16a63eea8cab11f4e62053f364da
Roman Gonzalez <roman.gonzalez@chivowallet.com>
2024-01-23 15:44:06 -0600

    feat: implement api

---
+++ src/api/votaciones.ts
@@ -0,0 +1,43 @@
+import data from "../data/votaciones.json";
+
+export interface FilterParams {
+  deparment?: string;
+  minucipality?: string;
+  votingCenter?: string;
+}
+
+export type Entries<T> = {
+  [K in keyof T]: [K, T[K]];
+}[keyof T][];
+
+export function find(params: FilterParams) {
+  const entries = Object.entries(params) as Entries<FilterParams>;
+
+  if (entries.length === 0) return data;
+
+  data.filter((item) => {
+    let condition = true;
+
+    for (const entry of entries) {
+      if (!entry) break
+
+      const value = entry[1]?.toUpperCase()
+
+      switch (entry[0]) {
+        case "deparment":
+          condition = condition && item.departamento === value;
+          break;
+
+        case "minucipality":
+          condition = condition && item.municipio === value;
+          break;
+
+        default:
+          condition = condition && item.centro_de_votacion === value;
+          break;
+      }
+    }
+
+    return condition;
+  });
+}
