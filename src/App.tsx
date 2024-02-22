f9d1b0eb58cf327241f3d82eba74809afc697bd5
DesarrolloChivoWallet <139510495+DesarrolloChivoWallet@users.noreply.github.com>
2024-02-22 14:26:35 -0600

    set tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 function App() {
 
   return (
-    <>
+    <div className='p-4'>
       {/* component */}
-      hello world
-    </>
+      <Tabs>
+        <TabList>
+          <Tab>Title 1</Tab>
+          <Tab>Title 2</Tab>
+        </TabList>
+
+        <TabPanel>
+          <h2>Any content 1</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 2</h2>
+        </TabPanel>
+      </Tabs>
+    </div>
   )
 }
 
