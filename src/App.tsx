84e9a793b67e922f86c4a903ca5d505bac237486
DesarrolloChivoWallet <139510495+DesarrolloChivoWallet@users.noreply.github.com>
2024-02-22 14:36:03 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
+import { useState, useEffect } from 'react';
 function App() {
+  const [panel1, setPanel1] = useState("");
+  const [panel2, setPanel2] = useState("");
+  const [panel3, setPanel3] = useState("");
+  useEffect(() => {
+    fetch('https://chivo-hub-dev.api.chivowallet.com/api/v1/')
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
 
   return (
     <div className='p-4'>
       {/* component */}
       <Tabs>
         <TabList>
-          <Tab>Title 1</Tab>
-          <Tab>Title 2</Tab>
+          <Tab>Panel 1</Tab>
+          <Tab>Panel 2</Tab>
+          <Tab>Panel 3</Tab>
         </TabList>
 
         <TabPanel>
@@ -16,6 +30,9 @@ function App() {
         </TabPanel>
         <TabPanel>
           <h2>Any content 2</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 3</h2>
         </TabPanel>
       </Tabs>
     </div>
